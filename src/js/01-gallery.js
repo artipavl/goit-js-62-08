9ba4c12fd4ac35278c5880f87b0ebdbc9c25ceb4
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-04 23:34:58 +0300

    Додає виконання 1 ДЗ

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,38 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const gallery = document.querySelector(".gallery");
+
+gallery.innerHTML = addGalleryItems(galleryItems);
+
+const lightBox = new SimpleLightbox(".gallery__item", {
+  enableKeyboard: true,
+  captionDelay: 250,
+});
+
+gallery.addEventListener("click", openLightBox)
+  
+function openLightBox(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  lightBox.overlay = true;
+}
+
+function addGalleryItems(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+                <a class="gallery__item" href="${original}">
+                    <img class="gallery__image" src="${preview}" alt="${description}" title ="${description}" />
+                </a>
+            `;
+    })
+    .join("");
+}
