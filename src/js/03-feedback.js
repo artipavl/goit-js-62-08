561ee51d19a38d95e566e65ffc6bb1f814c5671a
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-05 16:20:25 +0300

    Додає виконання 3 ДЗ

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+const formEl = document.querySelector('.feedback-form');
+const emailFormEl = document.querySelector('[name="email"]');
+const messageFormEl = document.querySelector('[name="message"]');
 
+formEl.addEventListener('input', setInputFormInLH);
+formEl.addEventListener('submit', submitForm);
+
+let formLog = {};
+
+if (localStorage.getItem('feedback-form-state')) {
+  formLog = JSON.parse(localStorage.getItem('feedback-form-state'));
+  emailFormEl.value = formLog.email;
+  messageFormEl.value = formLog.message;
+}
+
+function setInputFormInLH(e) {
+  formLog[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(formLog));
+}
+
+function submitForm(e) {
+  e.preventDefault();
+
+  const {
+    elements: { email, message },
+  } = e.currentTarget;
+
+  if (email.value === '' || message.value === '') {
+    return alert('Заповніть поля');
+  }
+  // Створив окремий об'єкт для формування даних з форми
+  // міг використовувати для цього formLog (не став використовувати згадавши про поле паролю, та вирішив що доцільно використовувати інший об’єкт )
+
+  const userData = {
+    email: email.value,
+    message: message.value,
+  };
+
+  console.log(userData);
+  //   console.log(formLog);
+
+  e.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
