a4d4ccb99f9fdf25d889ace9b42a63dc3858e8ce
artipavl <40296816+artipavl@users.noreply.github.com>
2022-10-05 15:10:31 +0300

    Додає виконання 2 ДЗ

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const options = {
+  width: 640,
+  loop: true,
+};
+
+const player = new Player('vimeo-player', options);
+
+let playerTime = 0;
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  playerTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+  player.setCurrentTime(playerTime);
+}
+
+player.on('timeupdate', throttle(setPlayerSecondsinLS, 1000));
+
+function setPlayerSecondsinLS({seconds}) {
+  playerTime = seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(playerTime));
+}
